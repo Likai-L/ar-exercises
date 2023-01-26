26ba8ae45d954041965640c9aa4298fc2070a13c
Likai Liu <likailiu2@gmail.com>
2023-01-27 06:22:15 +0800

    Create some employees in store1 and store2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+puts Store.count
+pp Store
+pp Employee
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Taylor", last_name: "Swift", hourly_rate: 60)
+@store2.employees.create(first_name: "Lana", last_name: "Del Rey", hourly_rate: 60)
+@store2.employees.create(first_name: "Katy", last_name: "Perry", hourly_rate: 60)
