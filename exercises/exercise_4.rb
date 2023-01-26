9efa7cafbafed4dcadbbda1fcbe4de37f5b9adb9
Likai Liu <likailiu2@gmail.com>
2023-01-27 05:42:04 +0800

    Fetch data with the where method

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+stores1 = Store.where(mens_apparel: true);
+stores1.each do |store|
+  puts "#{store.name} #{store.annual_revenue}"
+end 
+
+stores2 = Store.where(womens_apparel: true, annual_revenue: (...1000000));
+stores2.each do |store|
+  puts "#{store.name} #{store.annual_revenue}"
+end
