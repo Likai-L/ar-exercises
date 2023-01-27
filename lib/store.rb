513a0a576026e8a521c8f05f198b91b780294ecc
Likai Liu <likailiu2@gmail.com>
2023-01-27 08:31:38 +0800

    Add validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :mens_or_womens
+  def mens_or_womens
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:womens_apparel, "men's appare and women's apparel can't be both false")
+    end
+  end
 
 end
